d98efb50bc2f03f1df281013dbe43ec103bdfea4
Ashley Barcel <139181625+AshleyBarcel-SMS@users.noreply.github.com>
2023-12-21 17:42:24 -0500

    first commit for crypto project

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+enum class Colors{blue, green, red};
+
+int main()
+{
+    std::string s = "hello";
+    float f = 1.5f;
+    
+    return 0;
+}
---
+++ test2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void printHello()
+{
+    std::cout << "Hello WORLD " << std::endl;
+}
+
+/** compute averate of x and y*/
+float average(float x, float y)
+{
+    return (x+y)/2;
+}
