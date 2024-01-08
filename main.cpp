52d02e4698e2e92f455d147db1a9a6c600f82eda
BintAmun <ashleyemanon@hotmail.com>
2024-01-08 13:55:33 -0500

    final commit from sms

--- main.cpp
+++ main.cpp
@@ -13,12 +13,13 @@ class OrderBookEntry
                         std::string _timestamp,
                         std::string _product,
                         OrderBookType _orderType)
+        : price(_price), //initializing the data
+          amount(_amount), 
+          timestamp(_timestamp),
+          product(_product),
+          orderType(_orderType)
         {
-            price = _price;
-            amount = _amount;
-            timestamp = _timestamp;
-            product = _product;
-            orderType = _orderType;
+           
         }
 
         double price;
@@ -125,54 +126,42 @@ void processUserOption(int userOption)
 
 int main()
 {
-    // double price = 5319.450228;
-    // double amount = 0.00020075;
-    // std::string timestamp{"2020/03/17 17:01:24.884492"};
-    // std::string product{"BTC/USDT"};
-    //std::string orderType{"bid"};
-
-    
-    //OrderBookType orderType = OrderBookType::ask;
-    // std::vector<double> prices;
-    // std::vector<double> amounts;
-    // std::vector<std::string> timestamps;
-    // std::vector<std::string> products;
-    // std::vector<OrderBookType> orderTypes;
-
-    // prices.push_back(5000.01);
-    // amounts.push_back(0.001);
-    // timestamps.push_back("2020/03/17 17:01:24.884492");
-    // products.push_back("BTC/USDT");
-    // orderTypes.push_back(OrderBookType::bid);
-
-    // prices.push_back(10.01);
-    // amounts.push_back(0.001);
-    // timestamps.push_back("2020/03/17 17:01:24.884492");
-    // products.push_back("BTC/USDT");
-    // orderTypes.push_back(OrderBookType::ask);
-
-    // std::cout << "prices: " <<prices[0] << std::endl;
-    // std::cout << "prices: " <<prices[1] << std::endl;
-
     // while (true)
     // {
     //     printMenu();
     //     int userOption = getUserOption();
     //     processUserOption(userOption);
     // }
-    OrderBookEntry order1{1000, 
+
+    std::vector<OrderBookEntry> orders;
+
+    orders.push_back(OrderBookEntry{1000, 
                         0.02, 
                         "2020/03/17 17:01:24.884492", 
                         "BTC/USDT", 
-                        OrderBookType::bid};
-    // order1.price = 10000;
-    // order1.amount = 0.002;
-    // order1.timestamp = "2020/03/17 17:01:24.884492";
-    // order1.product = "BTC/USDT";
-    // order1.orderType = OrderBookType::bid;
+                        OrderBookType::bid}     );
 
-    std::cout << "The price is " << order1.price << std::endl;
+    orders.push_back(OrderBookEntry{2000, 
+                    0.02, 
+                    "2020/03/17 17:01:24.884492", 
+                    "BTC/USDT", 
+                    OrderBookType::bid}     );                    
 
+    
+    //with & sybmol - no copy made; without it - copy made
+    //& iterating by reference
+    for (OrderBookEntry& order : orders)
+    {
+        std::cout << "The price is " << order.price << std::endl;
+    }
+    for (unsigned int i = 0; i < orders.size(); ++i )
+    {
+        std::cout << "The price is " <<orders[i].price << std::endl;
+    }
+    for (unsigned int i = 0; i < orders.size(); ++i )
+    {
+        std::cout << "The price is " <<orders.at(i).price << std::endl;
+    }
 
     return 0;
 }
